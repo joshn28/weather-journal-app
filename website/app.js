db9a7fe1ade1a0d6511a4edccb458ddf9b413dbd
Joshua Nobleza <joshnobleza@bell.net>
2023-05-10 15:05:32 -0400

    Integrate OpenWeatherMap API

--- website/app.js
+++ website/app.js
@@ -1,2 +1,19 @@
 // Personal API Key for OpenWeatherMap API
 const apiKey = '9efedeb2385a30b763768453145212fc&units=imperial';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+
+document.getElementById('generate').addEventListener('click', function() {
+    const zip = document.getElementById('zip').value;
+    getWeatherData(zip);
+});
+
+const getWeatherData = async (zip) => {
+    const response = await fetch(baseURL+zip+',us&appid='+apiKey);
+
+    try {
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log('error', err);
+    }
+};
